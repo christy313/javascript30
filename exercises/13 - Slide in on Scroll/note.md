45995418f659acef2c010e34459577c9421d6990
christy313 <christycreativity@gmail.com>
2022-08-24 17:39:18 -0400

    finish day13

--- exercises/13 - Slide in on Scroll/note.md
+++ exercises/13 - Slide in on Scroll/note.md
@@ -1,11 +1,23 @@
 ## Day13 Slide in on Scroll
 
 ### Summary
-
+While scrolling down at the half way of every image but not to the image bottom, add a class named `active` to show the hidden image.
 
 ### Practice
 
-#### 1. 
+#### 1. Write a function `checkSlide`, add event listener `scroll`, and wrap the function in `debounce` function to prevent too many scrolling functions evoking.
+
+#### 2. Find out the height of each conditions
+
+- a. `slideInAt` variable is for when sliding the half height of an image, trigger
+
+- b. Calculate the height form the top until every image bottom.
+
+- c. What I have learned in this app
+
+  - c.1 window methods: scrollY, innerHeight
+
+  - c.2 image: height
 
 ### Reference
 
--- exercises/13 - Slide in on Scroll/scroll.js
+++ exercises/13 - Slide in on Scroll/scroll.js
@@ -1,15 +1,36 @@
 function debounce(func, wait = 20, immediate = true) {
-  var timeout;
+  let timeout;
+
   return function () {
-    var context = this,
+    let context = this,
       args = arguments;
-    var later = function () {
+
+    function later() {
       timeout = null;
       if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
+    }
+    const callNow = immediate && !timeout;
+
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
+
     if (callNow) func.apply(context, args);
   };
 }
+
+const images = document.querySelectorAll(".slide-in");
+
+function checkSlide() {
+  images.forEach((image) => {
+    const slideInAt = window.scrollY + window.innerHeight - image.height / 2;
+    const imageBottom = image.offsetTop + image.height;
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    isHalfShown && isNotScrolledPast
+      ? image.classList.add("active")
+      : image.classList.remove("active");
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
