0cd61d38d039747086779d0710fe756ec3c7b279
christy313 <christycreativity@gmail.com>
2022-08-26 16:43:51 -0400

    finish day15

--- exercises/15 - LocalStorage/script.js
+++ exercises/15 - LocalStorage/script.js
@@ -1,6 +1,9 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
-const items = [];
+const clearAllButton = document.querySelector(".clear-all");
+const checkAllButton = document.querySelector(".check-all");
+const uncheckAllButton = document.querySelector(".uncheck-all");
+const items = JSON.parse(localStorage.getItem("items")) || [];
 
 function addItem(e) {
   e.preventDefault();
@@ -12,6 +15,7 @@ function addItem(e) {
 
   items.push(item);
   populateList(items, itemsList);
+  localStorage.setItem("items", JSON.stringify(items));
   this.reset();
 }
 
@@ -20,11 +24,28 @@ function populateList(plates = [], platesList) {
     .map((plate, i) => {
       return `
         <li>
-          <label for="">${plate.text}</label>
+          <input type="checkbox" data-index=${i} id="item${i}" ${
+        plate.done ? "checked" : ""
+      }>
+          <label for="item${i}">${plate.text}</label>
         </li>
-    `;
+      `;
     })
     .join("");
 }
 
+function toggleDone(e) {
+  if (!e.target.matches("input")) return;
+  const index = e.target.dataset.index;
+  items[index].done = !items[index].done;
+  populateList(items, itemsList);
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
 addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleDone);
+clearAllButton.addEventListener("click", clearAll);
+checkAllButton.addEventListener("click", checkAll);
+uncheckAllButton.addEventListener("click", uncheckAll);
+
+populateList(items, itemsList);
